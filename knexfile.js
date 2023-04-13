18c77429ad5c2714e353d397fbeaa65835a0ac41
kakun45 <email4blackkat@gmail.com>
2023-04-13 11:41:36 -0400

    knex config for deployment

--- knexfile.js
+++ knexfile.js
@@ -1,24 +1,25 @@
 require("dotenv").config();
 
-module.exports = {
-  client: "mysql2",
-  connection: {
-    host: "127.0.0.1",
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    charset: "utf8",
-  },
-};
-
-// phase 2 todo: for deployment
 // module.exports = {
 //   client: "mysql2",
 //   connection: {
-//     host: process.env.DATABASE_URL,
+//     host: "127.0.0.1",
 //     user: process.env.DB_USER,
 //     password: process.env.DB_PASSWORD,
-//     database: "isocrones",
+//     database: process.env.DB_NAME,
 //     charset: "utf8",
 //   },
 // };
+
+// phase 2 todo: for deployment
+module.exports = {
+  client: "mysql2",
+  connection: {
+    host: process.env.host,
+    user: process.env.username,
+    password: process.env.password,
+    database: process.env.database,
+    ssl: { rejectUnauthorized: process.env.ssl },
+    charset: "utf8",
+  },
+};
