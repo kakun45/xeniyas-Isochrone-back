00f1723f80acbaa041028c27d962fc09936805c7
kakun45 <email4blackkat@gmail.com>
2025-04-02 21:15:05 -0400

    knexfile.js fix

--- knexfile.js
+++ knexfile.js
@@ -4,27 +4,35 @@ const fs = require("fs");
 // console.log(data);
 // CLI: ```cat ca.pem | base64```
 
-const getSSLConfig = () => {
-  if (process.env.DATABASE_CA) {
-    return {
-      ca: Buffer.from(process.env.DATABASE_CA, "base64").toString("utf-8"),
-    };
-  }
-  return { rejectUnauthorized: false }; // Allow SSL without CA file
-};
-
 module.exports = {
   development: {
     client: "mysql2",
     connection: {
-      host: process.env.host,
-      user: process.env.username,
-      password: process.env.password,
-      database: process.env.database,
-      charset: "utf8",
-      ssl: {
-        ca: fs.readFileSync("ca.pem"), // Works locally when provided the actual file path to certif. When connected local-to-online db
-      },
+      // 25/3/31 works: from local server to local db
+      // host: process.env.host,
+      // user: process.env.username,
+      // password: process.env.password,
+      // database: process.env.database,
+
+      // 25/4/2 works: from local server to online db
+      host: process.env.DATABASE_HOST,
+      user: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      port: process.env.DATABASE_PORT,
+
+      // works, activate it
+      // ssl: {
+      //   ca: fs.readFileSync("ca.pem", "utf-8"), // Works locally when provided the actual file path to certif. When connected local-to-online db
+      // },
+
+      // testing. works locally
+      ssl: process.env.DATABASE_CA
+        ? {
+            // Directly passing the decoded base64 as a Buffer
+            ca: Buffer.from(process.env.DATABASE_CA, "base64"),
+          }
+        : { rejectUnauthorized: false },
     },
   },
   production: {
@@ -35,14 +43,19 @@ module.exports = {
       password: process.env.DATABASE_PASSWORD,
       database: process.env.DATABASE_NAME,
       port: process.env.DATABASE_PORT,
-      ssl: { rejectUnauthorized: false }, // Try without the CA
-      // ssl: getSSLConfig(), // Uses Base64 CA or allows SSL without strict CA check
+      ssl: process.env.DATABASE_CA
+        ? {
+            // Directly passing the decoded base64 as a Buffer
+            ca: Buffer.from(process.env.DATABASE_CA, "base64"),
+          }
+        : { rejectUnauthorized: false },
+      // ssl: { rejectUnauthorized: false }, // Try without the CA
     },
     charset: "utf8",
   },
 };
 
-// console.log(39, "LOG: knexfile.js:", process.env); // to see if .env is loaded
+console.log(39, "LOG: knexfile.js:", process.env.DATABASE_CA); // to see if .env is loaded
 // console.log("35) cert:", fs.readFileSync("ca.pem"));
 
 //  to create tables:
