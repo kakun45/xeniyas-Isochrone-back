8e6cc81c7aed98bacd76325a68d7c5e44a8cbcee
kakun45 <email4blackkat@gmail.com>
2026-09-25 19:14:03 -0400

    coment out unused lines

--- knexfile.js
+++ knexfile.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-const fs = require("fs");
+// const fs = require("fs");
 // const data = fs.readFileSync("ca.pem", "utf-8");
 // console.log(data);
 // CLI: ```cat ca.pem | base64```
@@ -54,7 +54,7 @@ module.exports = {
   },
 };
 
-console.log(39, "LOG: knexfile.js:", process.env.DATABASE_CA); // to see if .env is loaded
+// console.log(39, "LOG: knexfile.js:", process.env.DATABASE_CA); // to see if .env is loaded
 // console.log("35) cert:", fs.readFileSync("ca.pem"));
 
 //  to create tables:
